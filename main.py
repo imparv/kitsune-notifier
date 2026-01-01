ff0ad64cfe36a664f863bab1bae852dd6930f14d
parv <iamparv.r@gmail.com>
2026-01-01 20:33:12 +0530

    fix

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ twilio_whatsapp_number = "whatsapp:" + os.environ["TWILIO_WHATSAPP_NUMBER"]
 my_whatsapp_number = "whatsapp:" + os.environ["MY_WHATSAPP_NUMBER"]
 
 def send_whatsapp_message(msg):
-    """Send WhatsApp message via Twilio sandbox."""
     client_twilio.messages.create(
         body=msg,
         from_=twilio_whatsapp_number,
@@ -25,7 +24,7 @@ def send_whatsapp_message(msg):
 intents = discord.Intents.default()
 intents.message_content = True
 
-target_id = os.environ["target_id"]
+target_id = int(os.environ["TARGET_ID"])
 client = discord.Client(intents=intents)
 
 @client.event
